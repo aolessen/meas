cd50971531f5cf78e04d33a8176245649809861a
gawbds <gawbds@gmail.com>
2017-02-04 14:52:39 -0800

    Update thr.js

--- thr.js
+++ thr.js
@@ -82,32 +82,35 @@ for (i=0; i<items.length; i++)
 
 /******************************* Touch and click ******************************/
 var c = "";
-var timer;
+var bounds;
 window.ontouchstart = function(event) {
+// window.onmousedown = function(event) {
   c = event.target.className
-  if (c in units || c == "material" || c == "item")
-    timer = setTimeout(function() {event.target.focus();}, 1000);
+  bounds = event.target.getBoundingClientRect();
 }
 
 window.ontouchend = function(event) {
-  if (timer) clearTimeout(timer);
-  var bounds = event.target.getBoundingClientRect();
+// window.onmouseup = function(event) {
   if (event.changedTouches[0].pageX < bounds.left) { // next setting
-    for (u in units) if (c == u) units[c].push(units[c].shift());
-    if (c == "material") material[0].push(material.shift());
-    if (c == "item") location.search = "?"+items.pop().name;
-  } else if (event.changedTouches[0].pageX > bounds.right) { // previous setting
+  // if (event.clientX < bounds.left) { // next setting
     for (u in units) if (c == u) units[c].unshift(units[c].pop());
     if (c == "material") material[0].unshift(material.pop());
     if (c == "item") location.search = "?"+items.shift().name;
+  } else if (event.changedTouches[0].pageX > bounds.right) { // previous setting
+  // } else if (event.clientX > bounds.right) { // previous setting
+    for (u in units) if (c == u) units[c].push(units[c].shift());
+    if (c == "material") material[0].push(material.shift());
+    if (c == "item") location.search = "?"+items.pop().name;
   } else if (event.changedTouches[0].pageY < bounds.top) { // next major setting
-    if (c == "material") material.push(material.shift());
-  } else if (event.changedTouches[0].pageY > bounds.bottom) { // previous major setting
+  // } else if (event.clientY < bounds.top) { // next major setting
     if (c == "material") material.unshift(material.pop());
+  } else if (event.changedTouches[0].pageY > bounds.bottom) { // previous major setting
+  // } else if (event.clientY > bounds.bottom) { // previous major setting
+    if (c == "material") material.push(material.shift());
   } else {
     return;
   }
-  update();
+  update(items[0]);
 }
 
 
